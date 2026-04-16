832819cf8758ea6758ce95e87d4b1d63f83386c1
Vic Lobins <vic.l@pomegranate.co.uk>
2026-04-16 10:18:22 +0100

    removed comments

--- backend/wordpress/wp-content/themes/nextpress-theme/functions.php
+++ backend/wordpress/wp-content/themes/nextpress-theme/functions.php
@@ -57,6 +57,65 @@ function purge_all_wp_caches() {
 add_action( 'init', 'purge_all_wp_caches' );
 
 //------------------------------------------------
+// ** DISABLE COMMENTS **
+//------------------------------------------------
+
+// Close comments on all post types
+add_filter( 'comments_open', '__return_false', 20, 2 );
+add_filter( 'pings_open', '__return_false', 20, 2 );
+
+// Hide existing comments
+add_filter( 'comments_array', '__return_empty_array', 10, 2 );
+
+// Remove comments from admin menu and dashboard
+function disable_comments_admin_menu() {
+    remove_menu_page( 'edit-comments.php' );
+}
+add_action( 'admin_menu', 'disable_comments_admin_menu' );
+
+// Remove comments from admin bar
+function disable_comments_admin_bar() {
+    if ( is_admin_bar_showing() ) {
+        remove_action( 'admin_bar_menu', 'wp_admin_bar_comments_menu', 60 );
+    }
+}
+add_action( 'init', 'disable_comments_admin_bar' );
+
+// Redirect any attempts to access the comments page
+function disable_comments_admin_redirect() {
+    global $pagenow;
+    if ( $pagenow === 'edit-comments.php' ) {
+        wp_redirect( admin_url() );
+        exit;
+    }
+}
+add_action( 'admin_init', 'disable_comments_admin_redirect' );
+
+// Remove comments support from all post types
+function disable_comments_post_types_support() {
+    $post_types = get_post_types();
+    foreach ( $post_types as $post_type ) {
+        if ( post_type_supports( $post_type, 'comments' ) ) {
+            remove_post_type_support( $post_type, 'comments' );
+            remove_post_type_support( $post_type, 'trackbacks' );
+        }
+    }
+}
+add_action( 'admin_init', 'disable_comments_post_types_support' );
+
+// Remove comments from dashboard widgets
+function disable_comments_dashboard() {
+    remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+}
+add_action( 'admin_init', 'disable_comments_dashboard' );
+
+// Remove comments link from admin bar new content menu
+function disable_comments_new_content_admin_bar( $wp_admin_bar ) {
+    $wp_admin_bar->remove_node( 'comments' );
+}
+add_action( 'admin_bar_menu', 'disable_comments_new_content_admin_bar', 999 );
+
+//------------------------------------------------
 // ** TAXONOMIES **
 //------------------------------------------------
 
